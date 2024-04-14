f20452f33c0acbbfa4ac3337792bf04863906b44
mishalalshahari <mishalalshahari@gmail.com>
2024-04-14 13:06:57 +0530

    added register functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,59 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Register() {
+
+  const [ formData, setFormData ] = useState({});
+  const [ error, setError ] = useState(null);
+  const [ loading, setLoading ] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/login');
+    } catch(error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
         Register
       </h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username'/>
-        <input type="text" placeholder='Email' className='border p-3 rounded-lg' id='email'/>
-        <input type="text" placeholder='Password' className='border p-3 rounded-lg' id='password'/>
-        <button className='bg-purple-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Register</button>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type="text" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='bg-purple-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Register'}
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Already a user? </p>
@@ -18,6 +61,7 @@ export default function Register() {
           <span className='text-blue-700'>Login</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-3'>{error}</p>}
     </div>
   )
 }
