619928c91f4ce0f2a73be0b954a90c0efb15a8e6
mishalalshahari <mishalalshahari@gmail.com>
2024-05-09 14:18:46 +0530

    added rent page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-purple-500'>Property</span>
-                    <span className='text-purple-700'>Wala</span>
+                    <span className='text-purple-500'>HouSE</span>
+                    <span className='text-purple-700'>LL</span>
                 </h1>
             </Link>
             <form className='bg-purple-100 p-3 rounded-lg flex items-center'>
